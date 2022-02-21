db21f564266e8af0aacbccf339b907a8ac6da4b1
c1chy1 <cichy08081987pp@gmail.com>
2022-02-21 20:46:02 +0100

    adding dist subtree

---
+++ js/impressum.f9acb8ce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["impressum"],{"761a":function(e,n,t){"use strict";t.r(n);var r=t("7a23"),s={class:"text-center lg:text-8xl font-raleway mt-5"};function o(e,n,t,o,c,a){return Object(r["C"])(),Object(r["g"])("h1",s,"Impressum")}var c={name:"Impressum",beforeMount:function(){window.scrollTo(0,0)}},a=t("6b0d"),u=t.n(a);const w=u()(c,[["render",o]]);n["default"]=w}}]);
+//# sourceMappingURL=impressum.f9acb8ce.js.map
